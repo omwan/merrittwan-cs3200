f4e3ea72823284ac663dc77ee861cc23f8c48dcb
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-11-17 12:54:05 -0500

    adding swagger config

---
+++ src/main/java/merrittwan/cs3200/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package merrittwan.cs3200.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Configuration for swagger API documentation.
+ *
+ * Created by olivi on 07/16/2017.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.ant("/**"))
+            .build()
+            .apiInfo(buildApiInfo());
+  }
+
+  private ApiInfo buildApiInfo() {
+    return new ApiInfoBuilder()
+            .contact(new Contact("Olivia Wan", "https://github.com/omwan", "wan.o@husky.neu.edu"))
+            .description("API documentation for Databases Fall 2017 Final Project")
+            .title("CS 3200 Final Project")
+            .build();
+  }
+}
