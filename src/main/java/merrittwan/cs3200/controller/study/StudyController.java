6acc5adc9aff1f6a39c6d4d48d84f32f1d11d06b
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-11-28 12:30:37 -0500

    updating zip field to string instead of int

---
+++ src/main/java/merrittwan/cs3200/controller/study/StudyController.java
@@ -0,0 +1,7 @@
+package merrittwan.cs3200.controller.study;
+
+/**
+ * Created by olivi on 11/28/2017.
+ */
+public class StudyController {
+}
--- src/main/java/merrittwan/cs3200/entity/Address.java
+++ src/main/java/merrittwan/cs3200/entity/Address.java
@@ -17,7 +17,7 @@ public class Address {
 
   private String state;
 
-  private int zip;
+  private String zip;
 
   public Address() {
 
@@ -55,11 +55,11 @@ public class Address {
     this.state = state;
   }
 
-  public int getZip() {
+  public String getZip() {
     return zip;
   }
 
-  public void setZip(int zip) {
+  public void setZip(String zip) {
     this.zip = zip;
   }
 }
